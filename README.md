ddffa7a04312cc2c6506642db3eb9f0b183b27f4
Andre Jarrell <mail@andrejarrell.com>
2021-03-30 22:50:06 +1000

    first release for listcord rpc

--- README.md
+++ README.md
@@ -1,42 +1,59 @@
-# Discord RPC
+# Listcord RPC
 
-> Discord Rich Presence enables you to further customize your status on Discord.
+<a href="https://listcord.ga">
+    <img src="https://discordapp.com/api/guilds/726798031965519873/widget.png?style=shield">
+</a>
 
-<img alt="rpc" src="https://raw.githubusercontent.com/andrejarrell/RPC/master/src/img/screenshot.jpg">
+### Listcord RPC enables you to further customize your status on Discord
+
+<img src="https://raw.githubusercontent.com/listcord/rpc/master/src/img/rpc.png">
+
+<img src="https://raw.githubusercontent.com/listcord/rpc/master/src/img/app.png">
 
 ## 🛠 Setup
 
-- [x] Create an application at [Discord's Developer Portal](https://discord.com/developers/applications)
+✔ Create an application at [Discord's Developer Portal](https://discord.com/developers/applications)
 
 `New Application` -> (Name it) -> `Create` -> `General Information` -> (Copy Client ID)
 
-- [x] Upload images you will use
+✔ Upload images you will use
 
 `Rich Presence` -> `Add Image(s)`
 
-- [x] Clone repository (Requires [Git](https://git-scm.com/downloads))
+✔ Download latest [release](https://github.com/listcord/rpc/releases) for Windows or Linux
 
-`git clone https://github.com/andrejarrell/rpc.git`
+<details>
+    <summary>👨‍💻 If you'd rather run from source...</summary>
+    <br>
+    
+✔ Clone repository (Requires [Git](https://git-scm.com/downloads))
 
-- [x] Enter directory and install dependencies (Requires [Node.js](https://nodejs.org/en/download))
+`git clone https://github.com/listcord/rpc.git`
+
+✔ Enter directory and install dependencies (Requires [Node.js v14](https://nodejs.org/en/download))
 
 `npm ci`
 
 `npm start`
 
-(Downloading Electron for the first time may time some time)
+> Downloading Electron for the first time may awhile
 
 ## ⚙ Settings
 
 Settings are saved in `config.json` 
 
-| Name | Type | Required | Default |
-| -- | -- | -- | -- |
-| `clientId` | `String` | `true` | N/A |
-| `timestamp` | `String` | `false` | `Disable` |
-| `state` | `String` | `true` | `Undefined` |
-| `details` | `String` | `true` | `Undefined` |
-| `largeImageKey` | `String` | `false` | `Undefined` |
-| `largeImageText` | `String` | `false` | `Undefined` |
-| `smallImageKey` | `String` | `false` | `Undefined` |
-| `smallImageText` | `String` | `false` | `Undefined` |
+| Name | Type | Required |
+| -- | -- | -- |
+| `clientId` | `String` | `true` |
+| `timestamp` | `String` | `false` |
+| `state` | `String` | `false` |
+| `details` | `String` | `false` |
+| `largeImageKey` | `String` | `false` |
+| `largeImageText` | `String` | `false` |
+| `smallImageKey` | `String` | `false` |
+| `smallImageText` | `String` | `false` |
+| `buttonOneText` | `String` | `false` |
+| `buttonOneUrl` | `String` | `false` |
+| `buttonTwoText` | `String` | `false` |
+| `buttonTwoUrl` | `String` | `false` |
+</details>
--- index.js
+++ index.js
@@ -1,77 +1,116 @@
-let rpc = require('discord-rpc');
-let config = require('./config.json');
-let package = require('./package.json');
-let { app, BrowserWindow, ipcMain } = require('electron');
-
-let production = process.env.NODE_ENV === 'production';
+let rpc = require('discord-rpc')
+let validator = require('validator')
+let config = require('./config.json')
+let { app, BrowserWindow, ipcMain } = require('electron')
 
 let createWindow = () => {
     global.window = new BrowserWindow({
-        width: 800,
-        height: 690,
+        width: 1000,
+        height: 1200,
         webPreferences: {
-            nodeIntegration: true
+            nodeIntegration: true,
+            contextIsolation: false
         }
-    });
-    window.loadFile('src/index.html');
-    window.setIcon('src/img/logo.png');
-    window.setTitle(`Discord RPC ${package.version}`);
-};
+    })
+    window.removeMenu()
+    window.loadFile('src/index.html')
+    window.setIcon('src/img/logo.png')
+    window.setTitle('Listcord RPC')
+}
 
-app.on('ready', createWindow);
+app.on('ready', createWindow)
 
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit();
-});
+    if (process.platform !== 'darwin') app.quit()
+})
 
 app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
-});
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+})
+
+let client = new rpc.Client({ transport: 'ipc' })
 
 let setActivity = config => {
-    client.setActivity({
-        state: config.state || 'Undefined',
-        details: config.details || 'Undefined',
-        largeImageKey: config.largeImageKey || 'Undefined',
-        largeImageText: config.largeImageText || 'Undefined',
-        smallImageKey: config.smallImageKey || 'Undefined',
-        smallImageText: config.smallImageText || 'Undefined',
-        startTimestamp: config.timestamp === 'Enable' ? Date.now() : null
-    });
+    let activity = {}
+    let {
+        state,
+        details,
+        timestamp,
+        largeImageKey,
+        largeImageText,
+        smallImageKey,
+        smallImageText,
+        buttonOneUrl,
+        buttonOneText,
+        buttonTwoUrl,
+        buttonTwoText
+    } = config
+    if (timestamp === 'Enable')
+        activity.timestamps = {
+            start: Date.now()
+        }
+    activity.state = state || undefined
+    activity.details = details || undefined
+    activity.assets = {
+        large_image: largeImageKey || undefined,
+        large_text: largeImageText || undefined,
+        small_image: smallImageKey || undefined,
+        small_text: smallImageText || undefined
+    }
+    if (buttonOneText && validator.isURL(buttonOneUrl) 
+        || buttonTwoText && validator.isURL(buttonTwoUrl))
+        activity['buttons'] = []
+    if (buttonOneText && buttonOneUrl)
+        validator.isURL(buttonOneUrl) ?
+            activity.buttons.push({
+                url: buttonOneUrl,
+                label: buttonOneText
+            }) : null
+    if (buttonTwoText && buttonTwoUrl)
+        validator.isURL(buttonTwoUrl) ?
+            activity.buttons.push({
+                url: buttonTwoUrl,
+                label: buttonTwoText
+            }) : null
+    
+    client.request('SET_ACTIVITY', {
+        activity,
+        pid: process.pid
+    })
     window.webContents.send('alert', {
         type: 'success',
         message: 'Rich Presence was updated!'
-    });
-};
+    })
+}
 
-let connect = config => {
-    global.client = new rpc.Client({ transport: 'ipc' });
+let login = config => {
     if (!config.clientId) {
         return window.webContents.send('alert', {
             type: 'warning',
             message: 'Make sure to supply a Client ID!'
-        });
+        })
     }
     client.login({ clientId: config.clientId })
-    client.once('ready', () => setActivity(config));
-};
+    client.once('ready', () => setActivity(config))
+}
 
 let update = (event, config) => {
     if (client.clientId !== config.clientId) {
-        client.destroy();
-        connect(config);
+        client.destroy()
+        login(config)
     } else {
-        setActivity(config);
+        setActivity(config)
     }
-};
+}
 
-ipcMain.on('update', update);
-ipcMain.on('ready', () => connect(config));
+ipcMain.on('update', update)
+ipcMain.on('ready', () => login(config))
 
-process.on('uncaughtException', error => console.log(error));
-process.on('unhandledRejection', () => {
+process.on('uncaughtException', console.error)
+process.on('unhandledRejection', reject => {
+    console.log(reject)
     window.webContents.send('alert', {
         type: 'danger',
         message: 'Could not connect to RPC!'
-    });
-});
+    })
+})
